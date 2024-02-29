31b6934acbd39075476138a8f35d21b7a9744b4f
andres-ordonezs <andresmw.89@gmail.com>
2024-02-28 21:05:46 -0500

    Edit & delete routes completed

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def add_new_user():
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     input_url = request.form['image_url']
+    # TODO: Change default URL
     image_url = input_url if input_url else '<ion-icon name="person-outline"></ion-icon>'
 
     user = User(first_name=first_name,
@@ -78,7 +79,30 @@ def show_edit_page(user_id):
     return render_template('user_edit.html', user=user)
 
 
-# @app.post('/users/<int:user_id>/edit')
+@app.post('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User.query.get_or_404(user_id)
+
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+
+    db.session.commit()
+
+    return redirect('/users')
 
 
-# @app.post('/users/<int:user_id>/delete')
+@app.post('/users/<int:user_id>/delete')
+def delete_user(user_id):
+
+    user = User.query.get_or_404(user_id)
+
+    db.session.delete(user)
+
+    db.session.commit()
+
+    return redirect('/users')
